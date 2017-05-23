59fe56ea6c7853ccc798857341969717bdd7d192
Эльвин Агарагимов < >
2017-05-23 20:39:57 +0300

    first

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'compass/import-once'
+
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+sprite_load_path = "img/sprite"
+relative_assets = true
+line_comments = false
+
+preferred_syntax = :scss
+output_style = :compact
+environment = :production
+
+sass_options = {:debug_info => false}
+sass_options = {:sourcemap => true}
